bd5f260a207cc7d4202dfd4be3941052d1a2f59d
Pedro77h <pedrohenriquearaujosoares25@gmail.com>
2023-07-12 17:43:08 -0300

    feat(create swagger config): add swagger configuration, custom config and database module

---
+++ apps/01-nestjs-baseline/src/app/docs/swagger.config.ts
@@ -0,0 +1,8 @@
+import { SwaggerConfig } from './swagger.interface';
+
+export const SWAGGER_CONFIG: SwaggerConfig = {
+  title: 'User Service',
+  description: 'api specs',
+  version: '1.0',
+  tags: [],
+};
--- apps/01-nestjs-baseline/src/main.ts
+++ apps/01-nestjs-baseline/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { createDocument } from './app/docs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  createDocument(app);
+
   await app.listen(3000);
 }
 bootstrap();
